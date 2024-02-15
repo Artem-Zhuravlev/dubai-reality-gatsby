70eb5ec463b98b3a703f11b92bcb221406f2db95
Artem <mstrjesm@gmail.com>
2024-02-15 22:18:13 +0200

    feat: add data output for cotent section

--- src/entities/ContentSection/ContentSection.tsx
+++ src/entities/ContentSection/ContentSection.tsx
@@ -2,19 +2,59 @@ import React, { memo } from 'react';
 import './ContentSection.scss';
 import { Blockquote } from 'components/Blockquote/Blockquote';
 import { ImageTextCol } from 'components/lists';
+import { graphql, useStaticQuery } from "gatsby";
+import { IGatsbyImageData } from 'gatsby-plugin-image';
 
 export const ContentSection = memo((props) => {
+  const data = useStaticQuery(graphql`
+    query {
+      markdownRemark(frontmatter: {name: {eq: "content"}}) {
+        frontmatter {
+          author
+          blockquote
+          items {
+            text
+            image {
+              childImageSharp {
+                gatsbyImageData(
+                  height: 300
+                  placeholder: BLURRED
+                  formats: [AUTO]
+                )
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const {
+    author,
+    blockquote,
+    items
+  } = data.markdownRemark.frontmatter
+
+
+  console.log(data, 'content')
+
   return (
     <section className='content-section section'>
       <div className="container">
         <Blockquote
           className='content-section__blockquote'
-          cite="“Don’t limit yourself. Many people limit themselves to what they think they can do. You can go as far as your mind lets you. What you believe, remember, you can achieve.”"
-          author="Mary Kay Ash"
+          cite={blockquote}
+          author={author}
         />
         <div className="content-section__row">
-          <ImageTextCol />
-          <ImageTextCol />
+          {
+            items && items.map((item: { text: string; image: IGatsbyImageData; }) => (
+              <ImageTextCol
+                text={item.text}
+                imageUrl={item.image}
+              />
+            ))
+          }
         </div>
       </div>
     </section>
